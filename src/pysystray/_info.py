ff835c5256749b4c4c5ac965869be367ed58e776
PlayerG9 <g9yout@gmail.com>
2021-11-07 13:19:13 +0100

    moved (currently) not supported platforms

--- src/pysystray/_info.py
+++ src/pysystray/_info.py
@@ -17,5 +17,5 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-__author__ = u'Moses Palmér'
+__author__ = u'PlayerG9'
 __version__ = (0, 18, 0)
