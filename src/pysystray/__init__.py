3c8b8f7e91802a0e6afbcc23be46b8df2bdd3af2
PlayerG9 <g9yout@gmail.com>
2021-11-07 13:26:09 +0100

    added info-import

--- src/pysystray/__init__.py
+++ src/pysystray/__init__.py
@@ -20,6 +20,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import os
 import sys
 
+from ._info import __author__, __version__
+
 
 def backend():
     """Returns the backend module.
